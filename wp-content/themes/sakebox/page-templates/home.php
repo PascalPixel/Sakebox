08fb989ad57d3bbc554b681ff6b2d4fb47b6f5e2
Pascal Verstegen <pascal@superpencil.com>
2014-04-24 13:02:06 +0200

    Featured Saké

--- wp-content/themes/sakebox/page-templates/home.php
+++ wp-content/themes/sakebox/page-templates/home.php
@@ -85,6 +85,37 @@ if ($post_image_id) {
 		  <img class="imgwrap" src="<?php bloginfo('template_directory'); ?>/img/barrels.jpg"/>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-sm-12">
+		  <div class="divider"><hr><span>Featured Saké</span></div>
+		</div>
+  </div>
+	<div class="row">
+		<div class="col-sm-4 col-md-3 col-lg-2">
+  		<div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle1.png"/></div><h5>Hishimasamune</h5><h5 class="thin">超辛口純米酒</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle2.png"/></div><h5>Tea Umeshu</h5><h5 class="thin">お茶入り梅酒</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle3.png"/></div><h5>Yamahai Daiginjyo</h5><h5 class="thin">山廃大吟醸</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle4.png"/></div><h5>Nansen</h5><h5 class="thin">南泉</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle5.png"/></div><h5>Hishimasamune</h5><h5 class="thin">超辛口純米酒</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle6.png"/></div><h5>Tea Umeshu</h5><h5 class="thin">お茶入り梅酒</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle7.png"/></div><h5>Yamahai Daiginjyo</h5><h5 class="thin">山廃大吟醸</h5></div>
+		</div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+		  <div class="shop-item"><div class="imgwrap"><h4 class="price">¥923</h4><img src="<?php bloginfo('template_directory'); ?>/img/bottles/bottle8.png"/></div><h5>Nansen</h5><h5 class="thin">南泉</h5></div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
